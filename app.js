dc39abc75169269d59850655a9b8a0a02ed6a349
shasidharjavvaji <javvajishasidhar@gmail.com>
2016-09-21 16:00:44 +0530

    Session 6 Assignment 3

---
+++ app.js
@@ -0,0 +1,13 @@
+var i = 0;
+var n = 1;
+for(n=1;n<=1000;n++){
+i=i+n;
+}
+console.log("For Loop Result is:"+i);
+var a = 0;
+var b = 1;
+while (b<=1000){
+	a=a+b;
+	b++;
+}
+console.log("While Loop Result is:"+a);
